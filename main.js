a15b581a58ea4d180fb2d273f8a4c2a0217b2e47
Salvador <salvador.frazao.quintas@gmail.com>
2021-09-08 21:34:49 +0100

    melhorada a funcionalidade de desenho com controlo dos eventos de clique

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("canvas");
   const canvasCtx = canvas.getContext("2d");
 
+  let clicking = false;
+
   canvas.addEventListener("mousedown", (event) => {
     const canvasX = event.pageX - canvas.offsetLeft;
     const canvasY = event.pageY - canvas.offsetTop;
 
+    clicking = true;
     canvasCtx.beginPath();
     canvasCtx.moveTo(canvasX, canvasY);
   });
@@ -13,9 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const canvasX = event.pageX - canvas.offsetLeft;
     const canvasY = event.pageY - canvas.offsetTop;
 
-    canvasCtx.lineTo(canvasX, canvasY);
-    canvasCtx.stroke();
+    if (clicking) {
+      canvasCtx.lineTo(canvasX, canvasY);
+      canvasCtx.stroke();
+    }
   });
-  canvas.addEventListener("mouseup", (event) => {});
+  canvas.addEventListener("mouseup", (event) => {
+    clicking = false;
+  });
   canvas.addEventListener("mouseout", (event) => {});
 });
